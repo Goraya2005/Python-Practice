770b1951682d1f9c5677cf91d2524e6d153904ac
Muhammad Naeem Iqbal <138737132+Goraya2005@users.noreply.github.com>
2023-11-11 05:30:59 +0500

    Add files via upload

---
+++ add_two_numbers.py
@@ -0,0 +1,3 @@
+a : int = 30
+b : int = 15
+print("The sum of a and b is", a + b)
---
+++ avg.py
@@ -0,0 +1,6 @@
+a : str = input("Enter first number: ")
+b : str  = input("Enter second number: ")
+a : int  = int(a)
+b : int  = int(b)
+avg = (a + b)/2
+print("The average of a and b is", avg)
